2bddf5f06afa7af6ed8c3ee6b55b87d7e58f4ed0
Pashkouski Anton <Wolendrang@gmail.com>
2015-10-27 23:45:03 +0300

    initial commit

---
+++ IAPD/Vebcam/Vebcam/CommandList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vebcam
+{
+    class CommandList
+    {
+        public static string Get(CommandType type)
+        {
+            switch (type)
+            {
+                case CommandType.Information:
+                    return "info";
+                case CommandType.Video:
+                    return "video";
+                case CommandType.Foto:
+                    return "foto";
+                case CommandType.Exit:
+                    return "off";
+            }
+            return null;
+        }
+    }
+
+    enum CommandType 
+    {
+        Information,
+        Video,
+        Foto,
+        Exit
+    }
+}
---
+++ IAPD/Vebcam/Vebcam/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+namespace Vebcam
+{
+    class Program
+    {
+        private static Mutex applicationMutex = null;
+
+        static void Main(string[] args)
+        {
+            bool isMainApplication = TryTakeMutex();
+
+            if (isMainApplication)
+            {
+                Application.ApplicationExit += ApplicationExitHandler;
+                Thread.Sleep(100000);
+            }
+            else if (args.Length > 0 && args[0] == CommandList.Get(CommandType.Exit))
+            {
+                KillIdenticalProcess();
+            }
+        }
+
+        private static bool TryTakeMutex()
+        {
+            bool createdNew;
+            string applicationGuid = Marshal.GetTypeLibGuidForAssembly(Assembly.GetExecutingAssembly()).ToString();
+            applicationMutex = new Mutex(true, applicationGuid, out createdNew);
+
+            return createdNew;
+        }
+
+        private static void KillIdenticalProcess()
+        {
+            var processesList = Process.GetProcesses();
+            var identicalProcess =  processesList.FirstOrDefault(p => p.ProcessName == "Vebcam");
+
+            if (identicalProcess != null)
+            {
+                identicalProcess.Kill();
+            }
+        }
+
+        private static void ApplicationExitHandler(object sender, EventArgs e)
+        {
+            using (var file = new StreamWriter(new FileStream("hello.txt", FileMode.OpenOrCreate)))
+            {
+                file.WriteLine("follow the white rabbit.");
+            }
+        }
+    }
+}
