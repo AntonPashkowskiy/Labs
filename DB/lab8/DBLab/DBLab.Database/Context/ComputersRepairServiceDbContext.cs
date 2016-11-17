c832f0e7262a5a5115c22c14e6160e148a81d0b0
Anton Pashkouski <Wolendrang@gmail.com>
2016-11-17 20:08:12 +0300

    initial commit

---
+++ DB/lab8/DBLab/DBLab.Database/Context/ComputersRepairServiceDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLab.Database.Context
+{
+    public class ComputersRepairServiceDbContext: DbContext
+    {
+        #region Ctor
+
+        public ComputersRepairServiceDbContext(string connectionString) : base(connectionString)
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Protected Methods
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            RegisterEntitiesConfiguration(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void RegisterEntitiesConfiguration(DbModelBuilder modelBuilder)
+        {
+
+        }
+
+        #endregion
+    }
+}
